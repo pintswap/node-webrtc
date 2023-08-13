a8386a77423b1fb476d247b90ac5d0b23f69c244
pintswap <dev@pintswap.exchange>
2023-08-12 19:30:10 -0500

    update

--- scripts/download-webrtc.sh
+++ scripts/download-webrtc.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+alias python='python2.7'
+
 set -e
 
 set -v
 
+WRTC_TMPDIR=$(dirname $0)/tmp
+mkdir $WRTC_TMPDIR
+ln -s $(which python2.7) $WRTC_TMPDIR/python
+PATH=$WRTC_TMPDIR:$PATH
+
 export PATH=$DEPOT_TOOLS:$PATH
 
+UPDATE_DEPOT_TOOLS_PATH=$(dirname $0)/../build/external/depot_tools/src/update_depot_tools
+echo '#!/bin/bash' | tee $UPDATE_DEPOT_TOOLS_PATH
+echo "echo \"skipping update\"" | tee -a $UPDATE_DEPOT_TOOLS_PATH
+chmod +x $UPDATE_DEPOT_TOOLS_PATH
 gclient config --unmanaged --spec 'solutions=[{"name":"src","url":"https://webrtc.googlesource.com/src.git"}]'
 
 gclient sync --shallow --no-history --nohooks --with_branch_heads -r ${WEBRTC_REVISION} -R
